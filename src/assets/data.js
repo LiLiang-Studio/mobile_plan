de83fc5aa7aea9849ab187caa364fe3de1f9293d
liliang <ll917274996@live.com>
2022-10-11 15:58:53 +0800

    修复图片路径问题

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,32 +1,34 @@
+const getSrc = path => [process.env.base || '/', path].join('/')
+
 export default [
   {
     id: 1,
     title: '充值管理',
-    banner: '/005.jpg',
+    banner: getSrc('005.jpg'),
     items: ['我要充值', '话费查询']
   },
   {
     id: 2,
     title: '5G套餐',
-    banner: '/004.jpg',
+    banner: getSrc('004.jpg'),
     items: ['5G卡', '5G套餐']
   },
   {
     id: 3,
     title: '智慧家庭',
-    banner: '/002.jpg',
+    banner: getSrc('002.jpg'),
     items: ['宽带业务', '移动看家', '家庭共享群']
   },
   {
     id: 4,
     title: '智能终端',
-    banner: '/003.jpg',
+    banner: getSrc('003.jpg'),
     items: ['智能手机', '智能手环', '智能门锁', '智能血压仪']
   },
   {
     id: 5,
     title: '用户服务',
-    banner: '/001.jpg',
+    banner: getSrc('001.jpg'),
     items: ['个人信息', '消费查询', '账单发票', '普法宣传']
   }
 ]
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const base = '/mobile_plan'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mobile_plan',
+  base,
+  define: {
+    'process.env.base': JSON.stringify(base)
+  },
   plugins: [vue()]
 })
